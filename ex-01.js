ce75ef4b2d6a6ffc0bbdbec5d3258b233a435710
Larvin <49764430+LarvinV@users.noreply.github.com>
2021-01-21 22:29:52 -0300

    Adicionados ex, 1 e 4

---
+++ ex-01.js
@@ -0,0 +1,2 @@
+let soma = (x, y) => console.log(`Soma ${x + y}, subtração ${x / y}, Divisão ${x / y}, multiplicação ${x * y}`);
+soma(2, 3);
---
+++ ex-04.js
@@ -0,0 +1,3 @@
+let div = (x, y) => `${x}/${y} Resultado: ${x / y} Resto: ${x % y}`;
+console.log(div(9, 3));
+console.log(div(4, 3));
