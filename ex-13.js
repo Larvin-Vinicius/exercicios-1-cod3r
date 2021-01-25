1858fae589091a81f8e6c6ad2d30c84418ad72d1
Larvin <49764430+LarvinV@users.noreply.github.com>
2021-01-25 13:14:58 -0300

    Adicionado exercício 13

---
+++ ex-13.js
@@ -0,0 +1,33 @@
+function diaUtil(dia) {
+    switch (dia) {
+        case 1:
+            console.log('Domingo, não é dia util');
+            break;
+        case 2:
+            console.log('Segunda, é dia util');
+            break;
+        case 3:
+            console.log('Terça, é dia util');
+            break;
+        case 4:
+            console.log('Quarta, é dia util');
+            break;
+        case 5:
+            console.log('Quinta, é dia util');
+            break;
+        case 6:
+            console.log('Sexta, é dia util');
+            break;
+        case 7:
+            console.log('Sabado, não é dia util');
+        break;
+    }
+}
+
+diaUtil(1);
+diaUtil(2)
+diaUtil(3)
+diaUtil(4)
+diaUtil(5)
+diaUtil(6)
+diaUtil(7)
